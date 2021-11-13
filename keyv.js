2cfc74fad9cd56c2c1c79d5560450cec6d43cb28
thachhn3 <thachhn3@vng.com.vn>
2021-11-13 21:58:50 +0700

    update debug db

--- keyv.js
+++ keyv.js
@@ -3,6 +3,7 @@ const db = new jsoning('db.json');
 
 async function setData(data) {
 	try {
+		console.log('setData', db);
 		const { ago, duration, image, thumbnail, timestamp, title, url, videoId, views } = data.video;
 		await db.set(`${Date.now()}`, JSON.stringify({
 			music: { ago, duration, image, thumbnail, timestamp, title, url, videoId, views },
