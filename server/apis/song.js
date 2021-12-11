e46f7d6c6f0a251f05e2d189292e00b98a508ec7
thachhn3 <thachhn3@vng.com.vn>
2021-12-11 16:48:41 +0700

    update add stream audio

--- server/apis/song.js
+++ server/apis/song.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
 const Song = require("../../models/Song");
+const stream = require("youtube-audio-stream");
 
 router.get("/getSongs", async (req, res) => {
   const songs = await Song.find();
+
   res.json(songs);
+});
+router.get("/audio", async (req, res) => {
+  try {
+    // for await (const chunk of stream(
+    //  "http://youtube.com/watch?v=NbQ9HCr3IeY"
+    // )) {
+    //  res.write(chunk);
+    // }
+    stream("http://youtube.com/watch?v=NbQ9HCr3IeY").pipe(res);
+    // res.end();
+  } catch (err) {
+    console.error(err);
+    if (!res.headersSent) {
+      res.writeHead(500);
+      res.end("internal system error");
+    }
+  }
 });
 
 module.exports = router;
