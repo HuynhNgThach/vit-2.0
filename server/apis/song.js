3d3c58b9807b8d929634018af2b1b924834b7340
thachhn3 <thachhn3@vng.com.vn>
2021-12-12 07:48:46 +0700

    update add stream audio by id

--- server/apis/song.js
+++ server/apis/song.js
@@ -4,17 +4,23 @@ const stream = require("youtube-audio-stream");
 
 router.get("/getSongs", async (req, res) => {
   const songs = await Song.find();
-
-  res.json(songs);
+  const uniqSongs = [];
+  songs.forEach((song) => {
+    const existIndex = uniqSongs.findIndex((s) => s.id === song.id);
+    if (existIndex === -1) {
+      uniqSongs.push(song);
+    }
+  });
+  res.json(uniqSongs);
 });
-router.get("/audio", async (req, res) => {
+router.get("/audio/:videoID", async (req, res) => {
   try {
     // for await (const chunk of stream(
     //  "http://youtube.com/watch?v=NbQ9HCr3IeY"
     // )) {
     //  res.write(chunk);
     // }
-    stream("http://youtube.com/watch?v=NbQ9HCr3IeY").pipe(res);
+    stream(`http://youtube.com/watch?v=${req.params.videoID}`).pipe(res);
     // res.end();
   } catch (err) {
     console.error(err);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const api = require("./apis/song");
+const cors = require("cors");
 
 // app.listen(3030, () => {
 //  console.log(`Start server at port ${"3030"}`);
 // });
+app.use(cors());
 app.use("/vit", api);
 module.exports = app;
